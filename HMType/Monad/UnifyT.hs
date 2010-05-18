40653c8140145fee2e2336ab34306d5f2a0abb09
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2010-05-17 21:33:11 -0700

    Fix-up unification monad.

--- HMType/Monad/UnifyT.hs
+++ HMType/Monad/UnifyT.hs
@@ -10,19 +10,16 @@ import HMType.AST
 import HMType.Subst
 import Control.Monad.Fix
 
-newtype UnifyT tc k m a = I ( ExceptionT (MguError tc k)
-                            ( StateT (Subst tc k)
-                              m) a)
-      deriving (Functor,Monad,MonadFix)
+newtype UnifyT m a  = I (ExceptionT MguError (StateT Subst m) a)
+                        deriving (Functor,Monad,MonadFix)
 
-instance MonadT (UnifyT tc k) where
+instance MonadT UnifyT where
   lift m = I (lift (lift m))
 
-runUnifyT :: UnifyT tc k m a -> m (Either (MguError tc k) a, Subst tc k)
+runUnifyT :: UnifyT m a -> m (Either MguError a, Subst)
 runUnifyT (I m) = runStateT emptyS $ runExceptionT m
 
-unify :: (Monad m, Eq tc, KindOf tc k) =>
-          HMType tc k -> HMType tc k -> UnifyT tc k m ()
+unify :: Monad m => Type -> Type -> UnifyT m ()
 unify t1 t2 = I $
   do su1 <- get
      case mgu (apS su1 t1) (apS su1 t2) of
