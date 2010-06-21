7daced8cba01126f3ce7e81b2d5385a6e4f2f911
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2010-06-20 19:24:42 -0700

    Add natural number types.

--- Example/Type.hs
+++ Example/Type.hs
@@ -11,30 +11,40 @@ type Pred     = HM.Pred TCon
 type Kind     = HM.Kind TCon
 
 
-data TCon     = KStar | KFun
-              | TFun
+data TCon     = KStar | KFun | KNat
+              | TFun | TNat Integer
                 deriving (Eq,Show)
 
 instance KindOf TCon TCon where
-  kindOf KStar  = Nothing
-  kindOf KFun   = Nothing
-  kindOf TFun   = Just $ kFun kStar $ kFun kStar kStar
+  kindOf tcon =
+    case tcon of
+      KStar   -> Nothing
+      KFun    -> Nothing
+      KNat    -> Nothing
+      TFun    -> Just $ kFun kStar $ kFun kStar kStar
+      TNat n  -> if n >= 0 then Just kNat else Nothing
 
 instance IsTCon TCon
 
 instance PrettyCon TCon where
   ppCon _ r tcon args =
     case tcon of
-      KStar -> ppTApp r (char '*') args
-      KFun  -> ppFun
-      TFun  -> ppFun
-    where ppFun = case args of
-                    [t1,t2] -> wrap 5 r (t1 6 <+> text "->" <+> t2 5)
-                    _       -> ppTApp r (text "->") args
+      KStar   -> ppDoc (char '*')
+      KFun    -> ppFun
+      KNat    -> ppDoc (text "nat")
+      TFun    -> ppFun
+      TNat n  -> ppDoc (integer n)
+    where ppFun   = case args of
+                      [t1,t2] -> wrap 5 r (t1 6 <+> text "->" <+> t2 5)
+                      _       -> ppTApp r (text "->") args
+          ppDoc d = ppTApp r d args
       
 
 kStar        :: Kind
 kStar         = TCon KStar
+
+kNat         :: Kind
+kNat          = TCon KNat
 
 kFun         :: Kind -> Kind -> Kind
 kFun k1 k2    = TCon KFun `TApp` k1 `TApp` k2
